db9aac81a9691cb7bedee8ec5ae9b405e914eadb
Hugo Flores Garcia <hf01049@georgiasouthern.edu>
2021-03-23 15:48:42 -0500

    implement a tree class!

--- music_trees/partition.py
+++ music_trees/partition.py
@@ -1,5 +1,6 @@
 """partition.py - dataset partitioning"""
 import music_trees as mt
+from music_trees.tree import MusicTree
 
 from typing import List
 from collections import OrderedDict
@@ -8,16 +9,18 @@ import json
 import logging
 import random
 from pathlib import Path
+import copy
 
 from colorama import Fore
 from colorama import Style
 import medleydb as mdb
 
+DEPTH = 3
 TEST_SIZE = 0.3
 # these classes do not fit nicely in our hierarchy, either
 # because they're too general (horn section) or not an instrument (sampler)
-UNWANTED_CLASSES =  ('Main System', 'fx/processed sound', 'sampler', 'horn section',
-                     'string section', 'brass section', 'castanet', 'electronic organ', 'scratches')
+UNWANTED_CLASSES =  ('Main System', 'fx/processed sound', 'sampler', 'horn section', 
+                     'string section', 'brass section', 'castanet', 'electronic organ', 'scratches', 'theremin', )
 
 # because instrument sections are the same as the instrument, we want them to be considered 
 # as one instrument
@@ -28,6 +31,7 @@ REMAP_CLASSES = {'violin section': 'violin',
                  'flute section': 'flute', 
                  'clarinet section': 'clarinet', 
                  'cello section': 'cello'}
+
 
 def load_unique_instrument_list():
     # the first thing to do is to partition the MDB track IDs and stem IDs into only the ones we will use.
@@ -55,21 +59,31 @@ def get_files_for_instrument(instrument: str):
     return files
 
 # keep track of each instrument and its list of files
-FILES = {inst: get_files_for_instrument(inst) for inst in load_unique_instrument_list()}
+if not (mt.ASSETS_DIR / 'mdb-files.json').exists():
+    FILES = {inst: get_files_for_instrument(inst) for inst in load_unique_instrument_list()}
+    mt.utils.data.save_entry(FILES, mt.ASSETS_DIR / 'mdb-files.json')
+else:
+    FILES = mt.utils.data.load_entry(mt.ASSETS_DIR / 'mdb-files.json')
 
-def _flat_split(instruments: List[str], test_size: float):
+def add_track_information_to_tree(tree):
+    for node in tree.expand_tree():
+        node = tree[node]
+        if tree.is_leaf(node):
+            node.data['n_tracks'] = len(FILES[node.uid])
+        else:
+            node.data['n_tracks'] = sum([len(FILES[n.uid]) for n in tree.leaves(node.uid)])
+
+def _flat_split(leaves: List[mt.tree.MusicNode], test_size: float):
     """ split a list of instruments according to given test size"""
-
-    # get a counted subset of the instruments
-    instruments = {inst: len(FILES[inst]) for inst in instruments}
-    instruments = list(OrderedDict(sorted(instruments.items(), key=lambda x: x[1])).keys())
+    
+    leaves = sorted(leaves, key=lambda node: node.data['n_tracks'])
 
     # split the list of leaf nodes according to the test size
-    split_idx = int(round(test_size*len(instruments)))
+    split_idx = int(round(test_size*len(leaves)))
     
     partition = {}
-    partition['train'] = instruments[split_idx:]
-    partition['test'] = instruments[:split_idx]
+    partition['train'] = [l.uid for l in leaves[split_idx:]]
+    partition['test'] = [l.uid for l in leaves[:split_idx]]
     
     return partition
 
@@ -88,54 +102,75 @@ def _update_partition(this: dict, other: dict):
         this[key].extend(other[key])
     return this
 
-def _hierarchical_split(tree: dict, partition: dict, test_size: float):
-    for name, node in tree.items():
-        if isinstance(node, dict):
-            # recurse!
-            local_partition = _hierarchical_split(node, partition, test_size)
-            partition = _update_partition(partition, local_partition)
+def _hierarchical_split(tree: MusicTree, depth: int, test_size=TEST_SIZE):
+    # get all nodes at said depth
+    nodes = tree.all_nodes_at_depth(depth)
+    partition = {'train': [], 'test': []}
 
-        elif isinstance(node, list):
-            # only get the valid instruments
-            breakpoint()
-            node = [inst for inst in node if inst in FILES.keys()]
-            # make the split and update the partitions
-            local_partition = _flat_split(node, test_size)
-            partition = _update_partition(partition, local_partition)
+    for node in nodes:
+        leaves = [n for n in tree.leaves(node.uid)]
+        local_partition = _flat_split(leaves, test_size=test_size)
+        partition = _update_partition(partition, local_partition)
 
     return partition
 
-def _pretty_print_hierarchical_split(tree: dict, partition: dict, level: int):
-    for name, node in tree.items():
-        if isinstance(node, dict):
-            print(level * '\t' + f"{name}")
-            _pretty_print_hierarchical_split(node, partition, level+1)
-        elif isinstance(node, list):
-            colors = [Fore.CYAN, Fore.GREEN]
-            local_partition = {}
-            print(level * '\t' + f"{name}")
+def _prune_tree_to_available_instruments(tree):
+    available = load_unique_instrument_list()
+    all_tags = [n for n in tree.all_nodes()]
+    not_available = [n.uid for n in all_tags if n.uid not in available and tree.is_leaf(n)]
+    tree.remove_by_tags(not_available)
 
-            for (split, instruments), color in zip(partition.items(), colors): 
-                local_partition[split] = [i for i in instruments if i in node]
-                print(color, end='')
-                print((level+1) * '\t' + f"{local_partition[split]}")
-                print(Style.RESET_ALL, end='')
+def add_partition_data_to_tree(tree, partition):
+    for split, insts in partition.items():
+        for inst in insts:
+            node = tree.get_node(inst)
+            node.data.update({'partition': split, 'n_tracks': len(FILES[inst])})
+
+def make_partition_subtree(tree: MusicTree, partition: dict, split: str):
+    # make a new tree to keep the other one const
+    tree = MusicTree(tree=tree, deep=True)
+
+    def in_partition(t, n):
+        return (n.uid in partition[split] and t.is_leaf(n)) or not t.is_leaf(n)
+
+    partition_tree = tree.filter_tree(in_partition)
+    add_track_information_to_tree(partition_tree)
+
+    return partition_tree
 
 def hierarchical_split():
     """ write me
     should make a 70/30 split
     at the leaves of a tree
     """
-    # we will be using a new taxonomy
-    # taxonomy = mt.utils.data.load_entry(
-    #     mt.ASSETS_DIR / 'base-taxonomy.yaml', format='yaml')
     taxonomy = mdb.INST_TAXONOMY
+    tree = MusicTree.from_taxonomy(taxonomy)
 
-    partition = {'train': [], 'test': []}
-    partition = _hierarchical_split(taxonomy, partition, TEST_SIZE)
+    # shorten tree to desired depth
+    tree = tree.shorten(DEPTH)
 
-    _pretty_print_hierarchical_split(taxonomy, partition, 0)
+    # prune out unwanted classes
+    # remove these two categories we know we don't want
+    tree.remove_by_tags(['other', 'electric'])
+    _prune_tree_to_available_instruments(tree)
 
+    add_track_information_to_tree(tree)
+
+    tree = tree.even_depth()
+    partition = _hierarchical_split(tree, depth=DEPTH, test_size=TEST_SIZE)
+    
+    print(Fore.MAGENTA)
+    print('TRAIN')
+    train_tree = make_partition_subtree(tree, partition, 'train')
+    train_tree.show(data_property='n_tracks')
+    print(Style.RESET_ALL)
+
+    print(Fore.CYAN)
+    print('TEST')
+    test_tree = make_partition_subtree(tree, partition, 'test')
+    test_tree.show(data_property='n_tracks')
+    print(Style.RESET_ALL)
+    
     logging.info(f'number of training classes: {len(partition["train"])}')
     logging.info(f'number of test classes: {len(partition["test"])}')
 
--- music_trees/tree.py
+++ music_trees/tree.py
@@ -1,52 +1,202 @@
+from copy import deepcopy
+
 import medleydb as mdb
+import treelib
 from treelib.tree import Tree
+from treelib.node import Node
 
-class MusicTree:
 
-    def __init__(self, name: str):
-        self.name = name
-        self.parent = None
-        self.children = []
-    
-    def is_root(self):
-        return self.parent is None
-    
-    def is_leaf(self):
-        return self.children == []
+class DotDict(dict):
+    """dot.notation access to dictionary attributes"""
 
-    
+    __getattr__ = dict.get
+    __setattr__ = dict.__setitem__
+    __delattr__ = dict.__delitem__
 
-    def all_leaves(self):
-        leaves = []
-        for child in self.children:
-            if child.is_leaf(): 
-                leaves.append(child)
-            else: 
-                leaves.extend(child.all_leaves())
-        return leaves
+    def __deepcopy__(self, memo=None):
+        return DotDict(deepcopy(dict(self), memo=memo))
+
+class MusicNode(Node):
     
+    def __init__(self, uid: str = None, **kwargs):
+        if uid is not None:
+            kwargs['identifier'] = uid
+        super().__init__(**kwargs)
+        if self.data is None:
+            self.data = DotDict()
+
+    @staticmethod
+    def new_unique(other):
+        """ 
+        creates a new unique
+        instance, copying the data but adding a _ to its id
+        the pointers are not copied. 
+        """
+        new = MusicNode(uid=other.uid+'_')
+        new.data = deepcopy(other.data)
+        return new
+
+    @property
+    def uid(self):
+        return self.identifier 
+
     def __repr__(self):
-        if self.is_leaf():
-            return f"{self.name}"
-        else:
-            st = f"{self.name}"
-            return st +
+        return f"{self.uid}-{self.data}"
 
+    # def __deepcopy__(self, memo=None):
+    #     # make a deepcopy of self, but give it a new id
+    #     other = deepcopy(self, memo=memo)
 
-def make_tree(tree: Tree, taxonomy: dict):
-    for name, subtaxonomy in taxonomy.items():
-        if isinstance(subtaxonomy, dict):
-            Tree.add_node(name)
-            node.make_tree(subtaxonomy)
-            self.children.append(node)
-        elif isinstance(subtaxonomy, list):
-            leaves = [MusicTree(name) for name in subtaxonomy]
-            self.children.extend(leaves)
+class MusicTree(Tree):
+
+    def __init__(self, **kwargs):
+        kwargs['node_class'] = MusicNode
+        super().__init__(**kwargs)
+        
+        if not hasattr(self, 'root'):
+            self.create_node('root')
         else:
-            raise ValueError(f"expected either a list of dict: {taxonomy}")
+            if self.root is None:
+                self.create_node('root')
+
+    def depth(self, node=None):
+        return super().depth(node) + 1
+    
+    def create_node(self, uid: str, parent=None):
+        return super().create_node(identifier=uid, parent=parent)
+
+    def _r_make_tree(self, parent: Node, taxonomy: dict):
+        for name, subtaxonomy in taxonomy.items():
+            if isinstance(subtaxonomy, dict):
+                child = self.create_node(name, parent=parent)
+                self._r_make_tree(child, subtaxonomy)
+            elif isinstance(subtaxonomy, list):
+                child = self.create_node(name, parent=parent)
+                leaves = [self.create_node(inst, parent=child) for inst in subtaxonomy]
+            else:
+                raise ValueError(f"expected either a list of dict: {taxonomy}")
+
+    @staticmethod
+    def from_taxonomy(taxonomy: dict):
+        this = MusicTree(node_class=MusicNode)
+        root = this.root
+        this._r_make_tree(root, taxonomy)
+        return this
+    
+    def all_nodes_at_depth(self, depth: int):
+        """ returns a list of nodes at a given depth 
+            if get_shallow_leaves is True, also returns 
+            all leaf nodes shallower than depth
+        """
+        nodes = []
+        for node in self.all_nodes():
+            if self.depth(node) == depth:
+                nodes.append(node)
+        return nodes
+
+    def remove_by_tags(self, tags: list):
+        for tag in tags:
+            node = self.get_node(tag)
+            if node is not None:
+                self.remove_node(node.uid)
+        return self
+
+    def is_leaf(self, node):
+        return self.is_branch(node.uid) == []
+
+    def filter_tree(self, condition: callable):
+        """
+        creates a new subtree only for the nodes 
+        that satisfy the condition provided by the user
+
+        the input `fn` will be passed the tree and the node 
+        """
+        other = MusicTree(tree=self, deep=True)
+        for node in self.all_nodes():
+            # if the condition is not met, then remove this node
+            if not condition(other, node):
+                other.remove_node(node.uid)
+        return other
+
+    def amend_paths(self, amend_fn: callable):
+        paths = list(self.paths_to_leaves())
+        amends = [True for path in paths]
+        while not all([a == False for a in amends]):  
+            amends = []
+            for path in paths:
+                amend = amend_fn(path)
+                amends.append(amend)
+                if amend == True:
+                    break
+            # because we've changed the paths, we must update them
+            paths = list(self.paths_to_leaves())
+
+    def even_depth(self):
+        """ 
+        this is probably unnecessarily O(n^2)
+        
+        modifies the tree, such that
+        the length of all paths from the root node to any 
+        leaf node is equal. This is done by inserting a new
+        parent to the leaf node in each path, in a loop until
+        the uniform depth requirement is met 
+        """
+        max_depth = self.depth()
+
+        def expand_path_if_needed(path):
+            if len(path) < max_depth:
+                # get the leaf's parent
+                parent = self[path[-2]]
+                leaf = self[path[-1]]
+                # create a copy of it
+                new_parent = MusicNode.new_unique(parent)
+
+                # get the children BEFORE we insert our new node
+                children = self.children(parent.uid)
+                # insert it into the tree as a child to parent
+                self.add_node(new_parent, parent=parent)
+
+                # iterate though all the children of parent
+                for child in children:
+                    # move the leaf as a child to the new parent
+                    self.move_node(child.uid, new_parent.uid)
+                
+                return True
+            else:
+                return False
+
+        self.amend_paths(expand_path_if_needed)
+        return self
+
+    def shorten(self, depth: int):
+        """ remove parents from leaves until 
+        the maximum depth of the tree is the depth provided
+        """
+        # we wanna cut the parents so depth is +1
+        depth += 1 
+        def shorten_path_if_needed(path):
+            if len(path) > depth:
+                # link past the first parent
+                parent = path[-2]
+                self.link_past_node(parent)
+                return True
+            else:
+                return False
+
+        self.amend_paths(shorten_path_if_needed)
+        return self
+
+    def is_even(self):
+        """ 
+        returns true if the lengths of all paths from the root
+        to each leaf is of equal length
+        """
+        paths = self.paths_to_leaves()
+        len_ = len(paths[0])
+        return all([len(path) == len_ for path in paths])
 
 if __name__ == "__main__":
     taxonomy = mdb.INST_TAXONOMY
     
-    tree = Tree()
-    tree.add_ 
+    tree = MusicTree.from_taxonomy(taxonomy)
+    print([node.identifier for node in tree.all_nodes_at_depth(2)])
