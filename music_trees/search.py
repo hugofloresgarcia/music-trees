73f43dec551cc9a228947eb69dc96fb339087b6c
Hugo Flores Garcia <hf01049@georgiasouthern.edu>
2021-05-03 13:26:53 -0500

    incus

--- music_trees/search.py
+++ music_trees/search.py
@@ -73,6 +73,11 @@ CONFIGS = {
         'loss_alpha': 0.5,
         'loss_beta': 1,
         'height': tune.grid_search([0, 4]),
+    },
+    'height-v1': {
+        'loss_alpha': 0.5,
+        'loss_beta': 1,
+        'height': tune.grid_search([0, 1, 2, 3, 4]),
     }
 }
 
