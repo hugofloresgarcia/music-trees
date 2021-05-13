dee196f6e811da85d27ff6ef116d468fa269dd6a
hugofloresgarcia <hugofloresgarcia@u.northwestern.edu>
2021-05-12 19:19:13 -0500

    added a super seed

--- music_trees/core.py
+++ music_trees/core.py
@@ -22,7 +22,7 @@ nussl.utils.seed(SEED)
 
 logging.basicConfig(
     format='%(asctime)s,%(msecs)d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',
-    datefmt='%Y-%m-%d:%H:%M:%S', level=logging.INFO)]
+    datefmt='%Y-%m-%d:%H:%M:%S', level=logging.INFO)
 
 def super_seed():
     pl.seed_everything(mt.SEED)
